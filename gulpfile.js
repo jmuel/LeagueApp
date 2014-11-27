41dbdb2f8fe25cbe49671bb951b9bcb45c0f056a
jmuel <jmuel11@gmail.com>
2014-11-27 10:58:35 -0600

    A few small changes, added a default task to gulp and modified the requirejs import

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,5 @@ var gulp        = require('gulp'),
 gulp.task('bower-files', function() {
     return gulp.src(bower_files()).pipe(gulp.dest('js/libs/'));
 });
+
+gulp.task('default', ['bower-files']);
