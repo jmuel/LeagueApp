64e50c193884d082ab69fb051f125de2e84f5ea4
jmuel <jmuel11@gmail.com>
2014-11-28 13:09:51 -0600

    building some cool new gulp tasks for browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,33 @@
-var gulp        = require('gulp');
+var gulp        = require('gulp'),
+    browserify  = require('browserify'),
+    reactify    = require('reactify'),
+    source      = require('vinyl-source-stream'),
+    uglify      = require('gulp-uglify'),
+    buffer      = require('vinyl-buffer');
+
+gulp.task('default', ['browserify-min', 'browserify', 'watch']);
+
+gulp.task('browserify-min', function() {
+    var b = browserify();
+    b.transform(reactify);
+    b.add('./js/main.js');
+    return b.bundle()
+        .pipe(source('LeagueApp.min.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('browserify', function() {
+    var b = browserify();
+    b.transform(reactify);
+    b.add('./js/main.js');
+    return b.bundle()
+        .pipe(source('LeagueApp.js'))
+        .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('watch', function() {
+    gulp.watch(['js/**/*.js', 'js/**/*.json'], ['browserify']);
+});
+
